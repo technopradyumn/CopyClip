23bb82c731c7d6d5eaf026ec570f4cabfedfb7ca
technopradyum <technopradyum@gmail.com>
2026-01-18 23:10:09 +0530

    some bug fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.technopradyumn.copyclip"
         minSdk = 24
         targetSdk = flutter.targetSdkVersion
-        versionCode = 16
-        versionName = "1.2.4"
+        versionCode = 17
+        versionName = "1.3.0"
         multiDexEnabled = true
     }
 
